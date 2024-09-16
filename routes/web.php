227d456cbe2a94e9a288a95faa1eb4fdebb354a9
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-09-16 12:45:33 +0200

    chore: fix up default installation

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('portal', function(){
+    return view('portal');
+});
+
+Route::group(['group' => 'app', 'middleware' => ['auth']], function() {
+    
 });
