76784aedc1a0ae1b12106b53441cc51350c9f72e
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-09-24 12:21:35 +0200

    fix: require user profile reference to be unique

--- database/migrations/0001_01_01_000004_create_profiles_table.php
+++ database/migrations/0001_01_01_000004_create_profiles_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->unique()->constrained();
 
             $table->string('first_name', 45);
             $table->string('name_prefix', 20)->nullable();
