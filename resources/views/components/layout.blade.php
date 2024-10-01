8ba616334a8b2381ad62aca57fca2e46409d5a02
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-10-01 11:01:03 +0200

    feat: use sass for styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? config('app.name') }}</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     {{ $slot }}
