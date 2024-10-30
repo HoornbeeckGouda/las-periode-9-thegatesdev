aad8e5e12e80e567e9ec8b7a9a3663eb866f4d8e
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-10-30 10:13:14 +0100

    fix: user model can have multiple profiles

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -33,9 +34,9 @@ class User extends Authenticatable
     }
 
 
-    public function profile(): HasOne
+    public function profiles(): HasMany
     {
-        return $this->hasOne(Profile::class);
+        return $this->hasMany(Profile::class);
     }
 
     public function documents(): BelongsToMany
