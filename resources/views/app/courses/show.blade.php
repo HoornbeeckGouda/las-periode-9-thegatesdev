151123fd3b6d89f130e5b3111dc8cfda7429e7a9
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-11-09 17:04:25 +0100

    feat: improve course modal layout

--- resources/views/app/courses/show.blade.php
+++ resources/views/app/courses/show.blade.php
@@ -1,17 +1,22 @@
 @props(['course'])
 
 <x-part.modal>
-    <h3>Opleiding {{ "($course->prefix)" }} <b>{{ $course->name }}</b></h3>
-    <p>Laatst actief: {{ $course->lastCourseYear->name }}</p>
-    <x-part.collapse text='Vakken {{ "($course->subjects_count)" }}'>
-        <ul>
-            @foreach ($course->subjects as $subject)
-                <li><p>{{ $subject->name }}</p></li>
-            @endforeach
-        </ul>
-    </x-part.collapse>
-    <div class="flex row gap">
+    <header>
+        <h3>Opleiding {{ "($course->prefix)" }} <b>{{ $course->name }}</b></h3>
+    </header>
+    <main>
+        <p>Laatst actief: {{ $course->lastCourseYear->name }}</p>
+        <br>
+        <x-part.collapse text='Vakken {{ "($course->subjects_count)" }}'>
+            <ul>
+                @foreach ($course->subjects as $subject)
+                    <li><p>{{ $subject->name }}</p></li>
+                @endforeach
+            </ul>
+        </x-part.collapse>
+    </main>
+    <footer class="flex row gap">
         <button class="btn solid swap active">Bewerken</button>
         <button class="btn solid swap negative">Verwijderen</button>
-    </div>
+    </footer>
 </x-part.modal>
