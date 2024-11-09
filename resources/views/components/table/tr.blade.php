96650d4741eabe0af2cf8230a6239db86373987f
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-11-09 16:34:48 +0100

    feat: throttle modal opening

--- resources/views/components/table/tr.blade.php
+++ resources/views/components/table/tr.blade.php
@@ -1,5 +1,5 @@
 @props(['item_id', 'route'])
 
-<tr hx-trigger="click" hx-get="{{ route($route, $item_id) }}" hx-target="body" hx-swap="beforeend">
+<tr hx-trigger="click throttle:2000" hx-get="{{ route($route, $item_id) }}" hx-target="body" hx-swap="beforeend">
     {{ $slot }}
 </tr>
